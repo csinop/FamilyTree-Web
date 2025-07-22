672d07233d18f3c9beadeef6372551c85bd509d1
csinop <csinop.dev@gmail.com>
2025-07-22 12:41:23 +0300

    Added the ability to edit imported JSON document on the webpage. Added save functionality to instantly view the changes that were made. Added download functionality to download the modified JSON document.

--- FamilyTreeApp/obj/Debug/net9.0/FamilyTreeApp.AssemblyInfo.cs
+++ FamilyTreeApp/obj/Debug/net9.0/FamilyTreeApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamilyTreeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810a301dd251e7b3968f16ee3168512eb2adff6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fffc41570f3f75ab83f7f0d16f5a537433a905c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamilyTreeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamilyTreeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
